d4d02ce3d2cd02363080bc2d7adec7469a74255d
David Register <bdbcat@users.sourceforge.net>
2014-10-27 20:39:46 -0400

    Disable dcscreen build due to missing <GLES/gl2.h> file for Qt Android

--- src/qt/dcscreen.cpp
+++ src/qt/dcscreen.cpp
@@ -11,20 +11,20 @@
 #include "wx/dcscreen.h"
 #include "wx/qt/dcscreen.h"
 
-#include <QtWidgets>
-#include <QPixmap>
+//#include <QtWidgets>
+//#include <QPixmap>
 
 IMPLEMENT_ABSTRACT_CLASS(wxScreenDCImpl, wxWindowDCImpl)
 
 wxScreenDCImpl::wxScreenDCImpl( wxScreenDC *owner )
     : wxWindowDCImpl( owner )
 {
-    m_qtImage = new QImage(QApplication::primaryScreen()->grabWindow(QApplication::desktop()->winId()).toImage());
+//    m_qtImage = new QImage(QApplication::primaryScreen()->grabWindow(QApplication::desktop()->winId()).toImage());
 }
 
 wxScreenDCImpl::~wxScreenDCImpl( )
 {
-    delete m_qtImage;
+ //   delete m_qtImage;
 }
 
 void wxScreenDCImpl::DoGetSize(int *width, int *height) const
